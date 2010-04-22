fb0374d239cdc10bbc41ecfdf199cf0587e595e5
Alex <aimmig2@hotmail.com>
2010-04-22 16:54:45 +1000

    added comments for both the 2 arugment and 3 arugment CPUID functions, and fixed return type for 3 arugment CPUID function from mboolean to bool

--- src/mavencore/util/SystemArchitecture.cpp
+++ src/mavencore/util/SystemArchitecture.cpp
@@ -3,14 +3,45 @@
 #include <bitset>
 
 // System architecture information
+/** 
+* @author Alex <aimmig2@hotmail.com>
+* @url http://www.sandpile.org/ia32/cpuid.htm for more information on what each CPUID function does and what
+* each bit returned represents
+*/
+
 
 namespace util {
 
-	mboolean SystemArchitecture::CPUID(int Function, int bit, char reg) {
+	/**
+	 * 3 input CPUID function.
+	 *
+	 * @brief CPUID(int, int, char) function that returns a single bit from a register specified after running the cpuid function
+	 * specificed.
+	 *
+	 * @return a single bit, specified in the second arugment [bit] from the register specified in the
+	 * third arugment [reg] after executing the cpuid function specificed by the first arugment [function].
+	 *
+	 * @param Function the CPUID function you want to execute, this is inputted with hexadecimal
+	 * notation to make reading easier.
+	 *
+	 * @param bit the bit you want return, inputted in decimal notation range [0 - 31].
+	 *
+	 * @param reg the register you want to return range ['A', 'B', 'C', 'D'] that corrospond to the x86 
+	 * registers EAX, EBX, ECX and EDX.
+	 *
+	 * Example use, the below function returns true if the CPU supports SSE and false if it doesn't support SSE.
+	 *
+	 * @code
+     * CPUID(0x0000001, 25, 'D'); //returns the 25th bit, from the register EDX after running the cpuid function 0x0000001
+	 * @endcode
+	 *
+	 * @url http://en.wikipedia.org/wiki/CPUID for more information on the CPUID instruction.
+	 */
+	bool SystemArchitecture::CPUID(int Function, int bit, char reg) {
 		int A, B, C, D;
 
 		#ifdef WIN32
-		__asm (
+		asm (
 			 "mov %%eax, %4\n"
 			 "cpuid\n"
 			 "mov %0, %%eax\n"
@@ -54,7 +85,29 @@ namespace util {
 	
 		return false;
 	}
-	
+
+	/**
+	* 2 input CPUID function.
+	*
+	* @brief CPUID(int, char) function that returns a int that contains the contents of the register
+	* specificed in the second arugment [reg].
+	*
+	* @return a int, which corrosponds to the register specificed in the second arugment [reg].
+	*
+	* @param Function the CPUID function you want to execute, this is inputted with hexadecimal
+	* notation to make reading easier.
+	*
+    * @param reg the register you want to return range ['A', 'B', 'C', 'D'] that corrospond to the x86 
+	* registers EAX, EBX, ECX and EDX.
+	*
+	* Example use, the below function returns the total number of cores on a die
+	*
+	* @code
+	* CPUID(0x00000001, 'A'); //returns the contents of register 'A' [EAX], after running the cpuid function 0x00000001
+	* @endcode
+	*
+	* @url http://en.wikipedia.org/wiki/CPUID for more information on the CPUID instruction. 
+	*/
 	int SystemArchitecture::CPUID(int Function, char reg) {
 		int A, B, C, D;
 
@@ -143,11 +196,18 @@ namespace util {
 	mboolean SystemArchitecture::SSE42() {
 		return CPUID(0x0000001, 20, 'C');
 	}
+
+	/**
+	 * Returns true if the current cpu supports 16 byte cmp and exchange
+	 */
 	
 	mboolean SystemArchitecture::P16BATOMIC() {				
 		return CPUID(0x0000001, 13, 'C');
 	}
 
+	/**
+	* Returns true if the current cpu supports 64bit instructions
+	*/
 	mboolean SystemArchitecture::LONGMODE() {
 		return CPUID(0x80000001, 29, 'D');
 	}
@@ -160,6 +220,11 @@ namespace util {
 	mboolean SystemArchitecture::multiProcessorCapable() {
 		return CPUID(0x80000001, 19, 'D');
 	}
+
+	/**
+	 * Returns the number cpu's on the die, note that a single package may contain multiple dies
+	 * and this will return the results for only one of the dies.
+	 */
 
 	mint SystemArchitecture::totalCores() {
 		unsigned int ret = CPUID(0x00000001, 'A');
